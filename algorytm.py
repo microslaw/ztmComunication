6ae81012d0c34b9bccfb7db26d6d9bc82ba1d3f4
sgolebiewska <sandra.golbiewska@op.pl>
2023-01-12 18:50:31 +0100

    pppp

--- algorytm.py
+++ algorytm.py
@@ -1,11 +1,11 @@
-# K01:	S  ← Ø	zbiór S ustawiamy jako pusty
-# K02:	Q  ← wszystkie wierzchołki grafu	 
-# K03:	Utwórz n  elementową tablicę d	tablica na koszty dojścia
-# K04:	Utwórz n  elementową tablicę p	tablica poprzedników na ścieżkach
+# K01:	S  ← Ø	zbiór S ustawiamy jako pusty                                                                                       ok
+# K02:	Q  ← wszystkie wierzchołki grafu	                                                                                       ok
+# K03:	Utwórz n  elementową tablicę d	tablica na koszty dojścia                                                                  ok
+# K04:	Utwórz n  elementową tablicę p	tablica poprzedników na ścieżkach                                                          ok
 # K05	Tablicę d  wypełnij największą
-# wartością dodatnią	 
-# K06:	d [ v  ] ← 0	koszt dojścia do samego siebie jest zawsze zerowy
-# K07:	Tablicę p  wypełnij wartościami -1	-1 oznacza brak poprzednika
+# wartością dodatnią	                                                                                                           ok
+# K06:	d [ v  ] ← 0	koszt dojścia do samego siebie jest zawsze zerowy                                                          ok
+# K07:	Tablicę p  wypełnij wartościami -1	-1 oznacza brak poprzednika                                                            ok
 # K08:	Dopóki Q  zawiera wierzchołki,
 # wykonuj kroki K09...K12	 
 # K09:	    Z Q  do S  przenieś wierzchołek u
@@ -19,3 +19,27 @@
 #             d [ w  ] ← d [ u  ] + waga krawędzi u–w
 #             p [ w  ] ← u	sprawdzamy koszt dojścia. Jeśli mamy niższy, to modyfikujemy koszt i zmieniamy poprzednika w na u
 # K13:	Zakończ
+
+
+import 
+
+numberBig=1000
+busStops=100
+
+checked_bus_id=set() #zbior Q
+
+access_costs=list()  #tablica d
+
+was_on_path=list()  #tablica p
+
+#wypelnic d najwieksza wartoscia dodatnia
+
+for i in range(busStops):
+    access_costs[i] = numberBig
+
+for i in range(busStops):
+    was_on_path[i] = -1
+
+startId = input() #name of the bus stop/ id
+access_costs(id)=0
+
