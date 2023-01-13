431b04ffa10fefbd7794a5086a188c867f33f7c2
IwsonHD <iwo@czart.net>
2023-01-13 05:18:11 +0100

    nie wielkie zmiany

--- front_controller.py
+++ front_controller.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template,request
+from datetime import datetime
 app=Flask(__name__)
 
 @app.route("/")
@@ -7,6 +7,23 @@ app=Flask(__name__)
 def main():
     return render_template("HACKATHON.html")
 
+@app.route("/pass_args" ,methods=['GET', 'POST'])
+
+def pass_args():
+    if request.method == 'POST':
+        first_stop = request.form['first_stop']
+        last_stop = request.form['last_stop']
+        date = datetime.strptime(request.form['date'],
+                                "%T-%m-%d")
+        time = request.form["time"]
+
+    else:
+        return render_template("HACKATHON.html")
+        
+@app.errorhandler(404)
+def page_not_found_handler(e):
+    return render_template("HACKATHON.html")
+       
 
 if __name__ == "__main__":
     app.run(debug = True, host = "0.0.0.0", port=80)
